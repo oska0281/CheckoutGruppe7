39638adfb7d319bf9a2949eb2213bdbbc949719b
w3zz0 <ahtzxx@gmail.com>
2023-03-12 17:36:21 +0100

    Vitest and telephone

--- src/Checkout.tsx
+++ src/Checkout.tsx
@@ -260,7 +260,7 @@ const Checkout = (): JSX.Element => {
                 </label>
                 <label>
                     Telefon
-                    <input type="tel" name="phone" required/>
+                    <p>+45 <input type="tel" name="phone" placeholder='12 34 56 78' maxLength={11} minLength={8} required/></p>
                 </label>
                 <label>
                     E-mail
