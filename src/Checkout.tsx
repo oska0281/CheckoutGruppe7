d1e510ec6c8c89b3185fefb3e478b70df09ae895
christoffer fink <christo.fink.1@gmail.com>
2023-03-12 22:11:53 +0100

    clean

--- src/Checkout.tsx
+++ src/Checkout.tsx
@@ -196,11 +196,6 @@ const Checkout = (): JSX.Element => {
 
                                     {cart.find((i) => i.id === item.id)?.quantity === 0 && cart.length > 0 ? (
                                         <>
-                                            Tilføj {item.rebateQuantity} mere for {item.rebatePercent}% rabat
-                                            {item.upsellProductId && itemsData.find((i) => i.id === item.upsellProductId) && (
-                                                <button onClick={() => addToCart(item.upsellProductId!)}>Tilføj
-                                                    eventuelt {itemsData.find((i) => i.id === item.upsellProductId!)?.name}</button>
-                                            )}
                                         </>
                                     ) : (
                                         (item.rebateQuantity && item.rebatePercent) && cart.find((i) => i.id === item.id)?.quantity! < item.rebateQuantity! ? (
